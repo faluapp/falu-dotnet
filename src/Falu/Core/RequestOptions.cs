1322ba1b7628b5d510bc8d4ea43caa9c4920a7f6
Maxwell Weru <mburumaxwell@gmail.com>
2021-02-25 10:43:22 +0300

    Added RequestOptions

---
+++ src/Falu/Core/RequestOptions.cs
@@ -0,0 +1,24 @@
+﻿namespace Falu.Core
+{
+    /// <summary>
+    /// Options that can be included in all requests/operations.
+    /// </summary>
+    public class RequestOptions
+    {
+        /// <summary>
+        /// The value to use for idempotent requests.
+        /// This value is set in the <c>X-Idempotency-Key</c> request header.
+        /// It can only be set for requests that are not idempotent by default.
+        /// These are requests/operations that create new data or mutate existing data.
+        /// </summary>
+        public string IdempotencyKey { get; set; }
+
+        /// <summary>
+        /// The identifier of the workspace to target.
+        /// This is only required when using user account bearer token, as the user may have access to one or more workspaces.
+        /// For API key authentication, the key already identifies the workspace.
+        /// This value is set in the <c>X-Falu-Workspace-Id</c> request header.
+        /// </summary>
+        public string Workspace { get; set; }
+    }
+}

27a1b7c379f72960fd96ccb93b14bbfbfc52cb2f
Maxwell Weru <mburumaxwell@gmail.com>
2021-07-14 09:54:20 +0300

    Replace balance.updated -> payment_balances.updated

--- src/Falu/Extensions/EnumExtensions.cs
+++ src/Falu/Extensions/EnumExtensions.cs
@@ -5,19 +5,14 @@ namespace System.Collections.Generic
 {
     internal static class EnumExtensions
     {
-        public static string? GetEnumMemberAttrValue<T>(this T enumVal) where T : Enum
+        public static string GetEnumMemberAttrValueOrDefault<T>(this T enumVal) where T : Enum
         {
             var memInfo = typeof(T).GetMember(enumVal.ToString());
             var attr = memInfo.FirstOrDefault()?.GetCustomAttributes(false)
                               .OfType<EnumMemberAttribute>()
                               .FirstOrDefault();
 
-            return attr?.Value;
-        }
-
-        public static string GetEnumMemberAttrValueOrDefault<T>(this T enumVal) where T : Enum
-        {
-            return enumVal.GetEnumMemberAttrValue() ?? enumVal.ToString().ToLowerInvariant();
+            return attr?.Value ?? enumVal.ToString().ToLowerInvariant();
         }
     }
 }

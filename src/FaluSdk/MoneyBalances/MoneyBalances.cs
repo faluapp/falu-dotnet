21ea198c2d8ce7923aae214eb88ce4b54702f363
Maxwell Weru <mburumaxwell@gmail.com>
2021-08-14 12:11:23 +0300

    Added missing properties in MoneyBalances

--- src/FaluSdk/MoneyBalances/MoneyBalances.cs
+++ src/FaluSdk/MoneyBalances/MoneyBalances.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using Falu.Core;
+using System;
+using System.Collections.Generic;
 
 namespace Falu.Payments
 {
@@ -6,12 +8,27 @@ namespace Falu.Payments
     /// Funds that are available to be transferred.
     /// They are categorized by provider and currency.
     /// </summary>
-    public class MoneyBalances
+    public class MoneyBalances : IHasCreated, IHasUpdated, IHasWorkspaceId, IHasLive, IHasEtag
     {
+        /// <inheritdoc/>
+        public DateTimeOffset Created { get; set; }
+
+        /// <inheritdoc/>
+        public DateTimeOffset Updated { get; set; }
+
         /// <summary>
         /// Breakdown of balance by business code.
         /// The value is represented in the smallest currrency unit.
         /// </summary>
         public Dictionary<string, long>? Mpesa { get; set; }
+
+        /// <inheritdoc/>
+        public string? WorkspaceId { get; set; }
+
+        /// <inheritdoc/>
+        public bool Live { get; set; }
+
+        /// <inheritdoc/>
+        public string? Etag { get; set; }
     }
 }

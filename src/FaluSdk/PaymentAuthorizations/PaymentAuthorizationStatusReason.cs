acc890c5b3403270853fd9565e0c7bd502e84717
Maxwell Weru <mburumaxwell@gmail.com>
2021-08-27 14:12:41 +0300

    Added PaymentAuthorizationStatusReason

---
+++ src/FaluSdk/PaymentAuthorizations/PaymentAuthorizationStatusReason.cs
@@ -0,0 +1,29 @@
+﻿namespace Falu.PaymentAuthorizations
+{
+    /// <summary>
+    /// Reason for a given status of payment authorization.
+    /// </summary>
+    public enum PaymentAuthorizationStatusReason
+    {
+        /// <summary>
+        /// No authorization webhook endpoint is set.
+        /// </summary>
+        Default,
+
+        /// <summary>
+        /// Authorization webhook endpoint is invalid.
+        /// </summary>
+        Invalid,
+
+        /// <summary>
+        /// Authorization webhook endpoint was used to approve or decline.
+        /// </summary>
+        Realtime,
+
+        /// <summary>
+        /// Synchronous webhook delivery to the authorization webhook endpoint failed.
+        /// </summary>
+        Failed,
+    }
+
+}

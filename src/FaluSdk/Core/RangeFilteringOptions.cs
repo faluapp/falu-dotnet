1a4d99af18fd4a126000d413f0d1dbc55bc3687f
Maxwell Weru <mburumaxwell@gmail.com>
2021-09-01 20:54:13 +0300

    Change RangeFilteringOptions<T> to readonly struct

--- src/FaluSdk/Core/RangeFilteringOptions.cs
+++ src/FaluSdk/Core/RangeFilteringOptions.cs
@@ -7,27 +7,42 @@ namespace Falu.Core
     /// Standard options for range filtering.
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public sealed class RangeFilteringOptions<T> where T : struct, IComparable<T>, IEquatable<T>
+    public readonly struct RangeFilteringOptions<T> where T : struct, IComparable<T>, IEquatable<T>
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="lt">Option for less than (&lt;).</param>
+        /// <param name="lte">Option for less than or equal to (&lt;=).</param>
+        /// <param name="gt">Option for greater than (&gt;).</param>
+        /// <param name="gte">Option for greater than or equal to (&gt;=).</param>
+        public RangeFilteringOptions(T? lt = null, T? lte = null, T? gt = null, T? gte = null)
+        {
+            LessThan = lt;
+            LessThanOrEqualTo = lte;
+            GreaterThan = gt;
+            GreaterThanOrEqualTo = gte;
+        }
+
         /// <summary>
         /// Option for less than (&lt;).
         /// </summary>
-        public T? LessThan { get; set; }
+        public T? LessThan { get; init; }
 
         /// <summary>
         /// Option for less than or equal to (&lt;=).
         /// </summary>
-        public T? LessThanOrEqualTo { get; set; }
+        public T? LessThanOrEqualTo { get; init; }
 
         /// <summary>
         /// Option for greater than (&gt;).
         /// </summary>
-        public T? GreaterThan { get; set; }
+        public T? GreaterThan { get; init; }
 
         /// <summary>
         /// Option for greater than or equal to (&gt;=).
         /// </summary>
-        public T? GreaterThanOrEqualTo { get; set; }
+        public T? GreaterThanOrEqualTo { get; init; }
 
         internal IDictionary<string, string> PopulateQueryValues(string property, IDictionary<string, string> dictionary, Func<T, string> converter)
         {

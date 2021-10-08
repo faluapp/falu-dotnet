63dc4fb449b2b924095d68cd9d78f093ea7b2cbc
Blinkman14 <kenneth.mugo@tingle.software>
2021-10-08 13:48:37 +0300

    Added tests for the TransferReversalsServiceClient

---
+++ tests/FaluSdk.Tests/Clients/TransferReversalsServiceClientTests.cs
@@ -0,0 +1,129 @@
+﻿using Falu.Core;
+using Falu.TransferReversals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Tingle.Extensions.JsonPatch;
+using Xunit;
+
+namespace Falu.Tests.Clients
+{
+    public class TransferReversalsServiceClientTests : BaseServiceClientTests<TransferReversal>
+    {
+        public TransferReversalsServiceClientTests() : base(new()
+        {
+            Id = "trr_123",
+            Currency = "KES",
+            Amount = 1_000_00,
+            Created = DateTimeOffset.UtcNow,
+            Updated = DateTimeOffset.UtcNow,
+            TransferId = "tr_123",
+            WorkspaceId = WorkspaceId
+        }, "/v1/transfer_reversals") { }
+
+        [Theory]
+        [MemberData(nameof(RequestOptionsData))]
+        public async Task GetAsync_Works(RequestOptions options)
+        {
+            var handler = GetAsync_Handler(options);
+
+            await TestAsync(handler, async (client) =>
+            {
+                var response = await client.TransferReversals.GetAsync(Data!.Id!, options);
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.NotNull(response.Resource);
+                Assert.Equal(Data!.Id, response.Resource!.Id);
+            });
+        }
+
+        [Theory]
+        [MemberData(nameof(RequestOptionsWithHasContinuationTokenData))]
+        public async Task ListAsync_Works(RequestOptions options, bool hasContinuationToken)
+        {
+            var handler = ListAsync_Handler(hasContinuationToken, options);
+
+            await TestAsync(handler, async (client) =>
+            {
+                var opt = new TransferReversalsListOptions
+                {
+                    Count = 1
+                };
+
+                var response = await client.TransferReversals.ListAsync(opt, options);
+
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.NotNull(response.Resource);
+                Assert.Single(response.Resource);
+
+                if (hasContinuationToken) Assert.NotNull(response.ContinuationToken);
+                else Assert.Null(response.ContinuationToken);
+
+                var ev = response!.Resource!.Single();
+
+                Assert.Equal(Data!.Id, ev.Id);
+            });
+        }
+
+        [Theory]
+        [MemberData(nameof(RequestOptionsData))]
+        public async Task ListRecursivelyAsync_Works(RequestOptions options)
+        {
+            var handler = ListAsync_Handler(options: options);
+
+            await TestAsync(handler, async (client) =>
+            {
+                var opt = new TransferReversalsListOptions
+                {
+                    Count = 1
+                };
+
+                var results = new List<TransferReversal>();
+
+                await foreach (var item in client.TransferReversals.ListRecursivelyAsync(opt, options))
+                {
+                    results.Add(item);
+                }
+
+                Assert.Single(results);
+                var ev = results.Single();
+                Assert.Equal(Data!.Id, ev.Id);
+            });
+        }
+
+        [Theory]
+        [MemberData(nameof(RequestOptionsData))]
+        public async Task CreateAsync_Works(RequestOptions options)
+        {
+            var handler = CreateAsync_Handler(options);
+
+            await TestAsync(handler, async (client) =>
+            {
+                var model = new TransferReversalCreateRequest { TransferId = Data!.TransferId };
+                var response = await client.TransferReversals.CreateAsync(model, options);
+
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.NotNull(response.Resource);
+            });
+        }
+
+        [Theory]
+        [MemberData(nameof(RequestOptionsData))]
+        public async Task UpdateAsync_Works(RequestOptions options)
+        {
+            var handler = UpdateAsync_Handler(options);
+
+            await TestAsync(handler, async (client) =>
+            {
+                var document = new JsonPatchDocument<TransferReversalPatchModel>();
+                document.Replace(x => x.Description, "new description");
+
+                var response = await client.TransferReversals.UpdateAsync(Data!.Id!, document, options);
+
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.NotNull(response.Resource);
+            });
+        }
+    }
+}

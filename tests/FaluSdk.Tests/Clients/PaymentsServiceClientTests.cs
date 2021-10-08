89f4b47e20d0fb196f2e061cf3d4eb1388ef829e
Blinkman14 <kenneth.mugo@tingle.software>
2021-10-08 13:48:19 +0300

    Added tests for the PaymentsServiceClient

---
+++ tests/FaluSdk.Tests/Clients/PaymentsServiceClientTests.cs
@@ -0,0 +1,139 @@
+﻿using Falu.Core;
+using Falu.Payments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Tingle.Extensions.JsonPatch;
+using Xunit;
+
+namespace Falu.Tests.Clients
+{
+    public class PaymentsServiceClientTests : BaseServiceClientTests<Payment>
+    {
+        public PaymentsServiceClientTests() : base(new()
+        {
+            Id = "pa_123",
+            Currency = "kes",
+            Amount = 100_00,
+            Created = DateTimeOffset.UtcNow,
+            Updated = DateTimeOffset.UtcNow,
+            WorkspaceId = WorkspaceId
+        }, "/v1/payments") { }
+
+        [Theory]
+        [MemberData(nameof(RequestOptionsData))]
+        public async Task GetAsync_Works(RequestOptions options)
+        {
+            var handler = GetAsync_Handler(options);
+
+            await TestAsync(handler, async (client) =>
+            {
+                var response = await client.Payments.GetAsync(Data!.Id!, options);
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.NotNull(response.Resource);
+                Assert.Equal(Data!.Id, response.Resource!.Id);
+            });
+        }
+
+        [Theory]
+        [MemberData(nameof(RequestOptionsWithHasContinuationTokenData))]
+        public async Task ListAsync_Works(RequestOptions options, bool hasContinuationToken)
+        {
+            var handler = ListAsync_Handler(hasContinuationToken, options);
+
+            await TestAsync(handler, async (client) =>
+            {
+                var opt = new PaymentsListOptions
+                {
+                    Count = 1
+                };
+
+                var response = await client.Payments.ListAsync(opt, options);
+
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.NotNull(response.Resource);
+                Assert.Single(response.Resource);
+
+                if (hasContinuationToken) Assert.NotNull(response.ContinuationToken);
+                else Assert.Null(response.ContinuationToken);
+
+                var ev = response!.Resource!.Single();
+
+                Assert.Equal(Data!.Id, ev.Id);
+            });
+        }
+
+        [Theory]
+        [MemberData(nameof(RequestOptionsData))]
+        public async Task ListRecursivelyAsync_Works(RequestOptions options)
+        {
+            var handler = ListAsync_Handler(options: options);
+
+            await TestAsync(handler, async (client) =>
+            {
+                var opt = new PaymentsListOptions
+                {
+                    Count = 1
+                };
+
+                var results = new List<Payment>();
+
+                await foreach (var item in client.Payments.ListRecursivelyAsync(opt, options))
+                {
+                    results.Add(item);
+                }
+
+                Assert.Single(results);
+                var ev = results.Single();
+                Assert.Equal(Data!.Id, ev.Id);
+            });
+        }
+
+        [Theory]
+        [MemberData(nameof(RequestOptionsData))]
+        public async Task CreateAsync_Works(RequestOptions options)
+        {
+            var handler = CreateAsync_Handler(options);
+
+            await TestAsync(handler, async (client) =>
+            {
+                var model = new PaymentCreateRequest 
+                {
+                    Currency = Data!.Currency,
+                    Amount = Data!.Amount,
+                    Mpesa = new PaymentCreateRequestMpesa 
+                    {
+                        Phone = "+254722000000",
+                        Paybill = true,
+                        Reference = "5001"
+                    }
+                };
+
+                var response = await client.Payments.CreateAsync(model, options);
+
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.NotNull(response.Resource);
+            });
+        }
+
+        [Theory]
+        [MemberData(nameof(RequestOptionsData))]
+        public async Task UpdateAsync_Works(RequestOptions options)
+        {
+            var handler = UpdateAsync_Handler(options);
+
+            await TestAsync(handler, async (client) =>
+            {
+                var document = new JsonPatchDocument<PaymentPatchModel>();
+                document.Replace(x => x.Description, "new description");
+
+                var response = await client.Payments.UpdateAsync(Data!.Id!, document, options);
+
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.NotNull(response.Resource);
+            });
+        }
+    }
+}

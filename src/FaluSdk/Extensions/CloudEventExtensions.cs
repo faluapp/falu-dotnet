7c191701a92140abf05307a1fef6c40519739239
Maxwell Weru <mburumaxwell@gmail.com>
2022-01-14 13:51:34 +0300

    CloudEventDataPayload<T> should inherit from WebhookEventData<T>

--- src/FaluSdk/Extensions/CloudEventExtensions.cs
+++ src/FaluSdk/Extensions/CloudEventExtensions.cs
@@ -68,20 +68,8 @@ public static class CloudEventExtensions
         };
     }
 
-    internal class CloudEventDataPayload<TObject>
+    internal class CloudEventDataPayload<TObject> : WebhookEventData<TObject>
     {
-        /// <summary>
-        /// Object containing the API resource relevant to the event.
-        /// For example, a <c>money_balances.updated</c> event will have a full balance object.
-        /// </summary>
-        public TObject? Object { get; set; }
-
-        /// <summary>
-        /// Object containing the names of the properties that have changed, and their previous
-        /// values (sent along only with <c>*.updated</c> events).
-        /// </summary>
-        public TObject? Previous { get; set; }
-
         /// <summary>
         /// Information on the API request that instigated the event.
         /// </summary>

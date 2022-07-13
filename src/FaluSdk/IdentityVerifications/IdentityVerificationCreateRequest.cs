a3b507820f6de509f10e5ac0f5d7d0911f92fc67
Maxwell Weru <mburumaxwell@gmail.com>
2022-07-13 13:08:36 +0300

    Added models for IdentityVerification

---
+++ src/FaluSdk/IdentityVerifications/IdentityVerificationCreateRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace Falu.IdentityVerifications;
+
+/// <summary>
+/// Information for creating an identity verification.
+/// </summary>
+public class IdentityVerificationCreateRequest : IdentityVerificationPatchModel
+{
+    /// <summary>
+    /// A set of verification checks to be performed.
+    /// </summary>
+    public IdentityVerificationChecks? Checks { get; set; }
+
+    /// <summary>
+    /// The URL the user will be redirected to upon completing the verification flow.
+    /// </summary>
+    public string? ReturnUrl { get; set; }
+}
---
+++ src/FaluSdk/IdentityVerifications/IdentityVerificationOutputs.cs
@@ -0,0 +1,42 @@
+﻿using Falu.Core;
+
+namespace Falu.IdentityVerifications;
+
+///
+public class IdentityVerificationOutputs
+{
+    /// <summary>
+    /// The user’s verified id number type.
+    /// </summary>
+    public string? IdNumberType { get; set; }
+
+    /// <summary>
+    /// The user’s verified id number.
+    /// </summary>
+    public string? IdNumber { get; set; }
+
+    /// <summary>
+    /// The user’s verified first name.
+    /// </summary>
+    public string? FirstName { get; set; }
+
+    /// <summary>
+    /// The user’s verified last name.
+    /// </summary>
+    public string? LastName { get; set; }
+
+    /// <summary>
+    /// The user’s verified date of birth.
+    /// </summary>
+    public DateTimeOffset? Birthday { get; set; }
+
+    /// <summary>
+    /// The user’s other verified names.
+    /// </summary>
+    public List<string>? OtherNames { get; set; }
+
+    /// <summary>
+    /// The user’s verified address.
+    /// </summary>
+    public PhysicalAddress? Address { get; set; }
+}

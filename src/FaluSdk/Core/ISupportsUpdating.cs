7c3ac3c1ed4997a786347726566042ef9c4517fb
Maxwell Weru <mburumaxwell@gmail.com>
2022-07-13 12:24:10 +0300

    Added ISupportsUpdating<T, TP>

---
+++ src/FaluSdk/Core/ISupportsUpdating.cs
@@ -0,0 +1,22 @@
+﻿using Tingle.Extensions.JsonPatch;
+
+namespace Falu.Core;
+
+///
+public interface ISupportsUpdating<TResource, TResourcePatchModel>
+    where TResource : IHasId, TResourcePatchModel
+    where TResourcePatchModel : class
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="id">Unique identifier for the object.</param>
+    /// <param name="patch"></param>
+    /// <param name="options">Options to use for the request.</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task<ResourceResponse<TResource>> UpdateAsync(string id,
+                                                  JsonPatchDocument<TResourcePatchModel> patch,
+                                                  RequestOptions? options = null,
+                                                  CancellationToken cancellationToken = default);
+}

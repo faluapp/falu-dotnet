441047830e2d76666df1b98db7b53aca3e794c6d
Maxwell Weru <mburumaxwell@gmail.com>
2022-07-13 10:46:06 +0300

    Added tests for EvaluationReportsServiceClient

---
+++ tests/FaluSdk.Tests/Clients/EvaluationReportsServiceClientTests.cs
@@ -0,0 +1,86 @@
+﻿using Falu.Core;
+using Falu.Evaluations;
+using System.Net;
+using Xunit;
+
+namespace Falu.Tests.Clients;
+
+public class EvaluationReportsServiceClientTests : BaseServiceClientTests<EvaluationReport>
+{
+    public EvaluationReportsServiceClientTests() : base(new()
+    {
+        Id = "evr_123",
+        Created = DateTimeOffset.UtcNow,
+        Updated = DateTimeOffset.UtcNow,
+    }, "/v1/evaluations/evaluation_reports")
+    { }
+
+    [Theory]
+    [MemberData(nameof(RequestOptionsData))]
+    public async Task GetAsync_Works(RequestOptions options)
+    {
+        var handler = GetAsync_Handler(options);
+
+        await TestAsync(handler, async (client) =>
+        {
+            var response = await client.EvaluationReports.GetAsync(Data!.Id!, options);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.NotNull(response.Resource);
+            Assert.Equal(Data!.Id, response.Resource!.Id);
+        });
+    }
+
+    [Theory]
+    [MemberData(nameof(RequestOptionsWithHasContinuationTokenData))]
+    public async Task ListAsync_Works(RequestOptions options, bool hasContinuationToken)
+    {
+        var handler = ListAsync_Handler(hasContinuationToken, options);
+
+        await TestAsync(handler, async (client) =>
+        {
+            var opt = new EvaluationReportsListOptions
+            {
+                Count = 1
+            };
+
+            var response = await client.EvaluationReports.ListAsync(opt, options);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.NotNull(response.Resource);
+            Assert.Single(response.Resource);
+
+            if (hasContinuationToken) Assert.NotNull(response.ContinuationToken);
+            else Assert.Null(response.ContinuationToken);
+
+            var ev = response!.Resource!.Single();
+
+            Assert.Equal(Data!.Id, ev.Id);
+        });
+    }
+
+    [Theory]
+    [MemberData(nameof(RequestOptionsData))]
+    public async Task ListRecursivelyAsync_Works(RequestOptions options)
+    {
+        var handler = ListAsync_Handler(options: options);
+
+        await TestAsync(handler, async (client) =>
+        {
+            var opt = new EvaluationReportsListOptions
+            {
+                Count = 1
+            };
+
+            var results = new List<EvaluationReport>();
+
+            await foreach (var item in client.EvaluationReports.ListRecursivelyAsync(opt, options))
+            {
+                results.Add(item);
+            }
+
+            Assert.Single(results);
+            var ev = results.Single();
+            Assert.Equal(Data!.Id, ev.Id);
+        });
+    }
+}

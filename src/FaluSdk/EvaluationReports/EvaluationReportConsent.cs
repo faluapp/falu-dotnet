1d0e2710e9afe05b23baae14721eb94c8c82304e
Maxwell Weru <mburumaxwell@gmail.com>
2022-07-13 13:34:42 +0300

    Split EvaluationReport.cs into sensible files

---
+++ src/FaluSdk/EvaluationReports/EvaluationReportConsent.cs
@@ -0,0 +1,20 @@
+﻿namespace Falu.EvaluationReports;
+
+///
+public class EvaluationReportConsent
+{
+    /// <summary>
+    /// Time at which the user gave consent for the evaluation to be done.
+    /// </summary>
+    public DateTimeOffset Date { get; set; }
+
+    /// <summary>
+    /// IP address from which the user gave consent for the evaluation to be done.
+    /// </summary>
+    public string? IP { get; set; }
+
+    /// <summary>
+    /// User agent of the device (e.g. browser) from which the user gave consent for the evaluation to be done.
+    /// </summary>
+    public string? UserAgent { get; set; }
+}

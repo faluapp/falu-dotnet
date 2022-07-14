0c219f658c4cb742491fad9e9b0734b9953a7c5d
Maxwell Weru <mburumaxwell@gmail.com>
2022-07-14 15:13:47 +0300

    Added IdentityVerificationOptionsForSelfie and IdentityVerificationOptionsForVideo

--- src/FaluSdk/IdentityVerifications/IdentityVerificationOptions.cs
+++ src/FaluSdk/IdentityVerifications/IdentityVerificationOptions.cs
@@ -12,6 +12,16 @@ public class IdentityVerificationOptions
     /// Options for the document check.
     /// </summary>
     public IdentityVerificationOptionsForDocument? Document { get; set; }
+
+    /// <summary>
+    /// Options for the selfie check.
+    /// </summary>
+    public IdentityVerificationOptionsForSelfie? Selfie { get; set; }
+
+    /// <summary>
+    /// Options for the video check.
+    /// </summary>
+    public IdentityVerificationOptionsForVideo? Video { get; set; }
 }
 
 ///
@@ -33,4 +43,35 @@ public class IdentityVerificationOptionsForDocument
     /// If a user uploads a document which isn't one of the allowed types, it will be rejected.
     /// </summary>
     public List<string>? Allowed { get; set; }
+}
+
+///
+public class IdentityVerificationOptionsForSelfie
+{
+    /// <summary>
+    /// Disable image uploads, selfie images have to be captured using the device's camera.
+    /// </summary>
+    public bool Live { get; set; }
+}
+
+///
+public class IdentityVerificationOptionsForVideo
+{
+    /// <summary>
+    /// Disable uploads, videos have to be captured using the device's camera.
+    /// </summary>
+    public bool Live { get; set; }
+
+    /// <summary>
+    /// Face poses to be performed in the video recording.
+    /// It is recommended to leave this field unassigned for the server to
+    /// generate random values per verification for security purposes.
+    /// </summary>
+    public List<string>? Poses { get; set; }
+
+    /// <summary>
+    /// Numerical phrase to be recited in the video recording.
+    /// When not provided, the server generates a random one.
+    /// </summary>
+    public int? Recital { get; set; }
 }

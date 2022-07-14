374d464384a78844693c0072390d8a54fb1785df
Maxwell Weru <mburumaxwell@gmail.com>
2022-07-14 15:04:22 +0300

    Added IdentityVerificationReportVideo.Document

--- src/FaluSdk/IdentityVerificationReports/IdentityVerificationReportVideo.cs
+++ src/FaluSdk/IdentityVerificationReports/IdentityVerificationReportVideo.cs
@@ -4,6 +4,11 @@
 public class IdentityVerificationReportVideo : AbstractIdentityVerificationReportCheck
 {
     /// <summary>
+    /// Identifier of the file holding the image of the identity document used in this check.
+    /// </summary>
+    public string? Document { get; set; }
+
+    /// <summary>
     /// Identifier of the file holding the video used in this check.
     /// </summary>
     public string? Video { get; set; }

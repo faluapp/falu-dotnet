cdb63bdf2f3585f9345887a83de3fb23601b7259
Maxwell Weru <mburumaxwell@gmail.com>
2022-07-18 09:26:24 +0300

    Remove WebhookEndpointCreateRequest.Format

--- src/FaluSdk/Webhooks/WebhookEndpointCreateRequest.cs
+++ src/FaluSdk/Webhooks/WebhookEndpointCreateRequest.cs
@@ -5,8 +5,5 @@
 /// </summary>
 public class WebhookEndpointCreateRequest : WebhookEndpointPatchModel
 {
-    /// <summary>
-    /// The format to use for webhook requests.
-    /// </summary>
-    public string? Format { get; set; }
+    // intentionally left blank
 }

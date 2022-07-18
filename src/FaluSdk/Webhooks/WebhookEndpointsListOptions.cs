02fe59155a9001a74216db5727772eb8962368c6
Maxwell Weru <mburumaxwell@gmail.com>
2022-07-18 14:04:01 +0300

    Added missing filters when listing webhook endpoints and message streams

--- src/FaluSdk/Webhooks/WebhookEndpointsListOptions.cs
+++ src/FaluSdk/Webhooks/WebhookEndpointsListOptions.cs
@@ -5,5 +5,13 @@ namespace Falu.Webhooks;
 /// <summary>Options for filtering and pagination of webhook endpoints.</summary>
 public record WebhookEndpointsListOptions : BasicListOptions
 {
-    // intentionally left blank
+    /// <summary>Filter options for <see cref="WebhookEndpointPatchModel.Status"/> property.</summary>
+    public List<string>? Status { get; set; }
+
+    /// <inheritdoc/>
+    internal override void Populate(QueryValues values)
+    {
+        base.Populate(values);
+        values.Add("status", Status);
+    }
 }

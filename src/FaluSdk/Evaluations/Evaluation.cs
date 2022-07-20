da3770153864becb2c4d34307877b32fea2c6166
Maxwell Weru <mburumaxwell@gmail.com>
2022-07-20 10:07:20 +0300

    Remove obsolete evaluation types (#102)
    Remove `Scope`, `Scoring`, and `Statement` properties from `Evaluation` type alongside their backing types.

--- src/FaluSdk/Evaluations/Evaluation.cs
+++ src/FaluSdk/Evaluations/Evaluation.cs
@@ -11,12 +11,6 @@ public class Evaluation : EvaluationPatchModel, IHasId, IHasCurrency, IHasCreate
     /// <inheritdoc/>
     public string? Currency { get; set; }
 
-    /// <summary>
-    /// Scope of the evaluation.
-    /// </summary>
-    [Obsolete("Moved to Options")]
-    public string? Scope { get; set; }
-
     /// <inheritdoc/>
     public DateTimeOffset Created { get; set; }
 
@@ -34,12 +28,6 @@ public class Evaluation : EvaluationPatchModel, IHasId, IHasCurrency, IHasCreate
     public EvaluationScoringOptions? Options { get; set; }
 
     /// <summary>
-    /// Statement used for the evaluation.
-    /// </summary>
-    [Obsolete("Moved to Reports or Outputs")]
-    public Statement? Statement { get; set; }
-
-    /// <summary>
     /// The short-lived client secret used by front-end libraries to show an evaluation modal inside your app.
     /// This client secret expires after 24 hours and can only be used once.
     /// Don’t store it, log it, embed it in a URL, or expose it to anyone other than the user.
@@ -53,13 +41,6 @@ public class Evaluation : EvaluationPatchModel, IHasId, IHasCurrency, IHasCreate
     /// Don’t store it, log it, send it in emails or expose it to anyone other than the target user.
     /// </summary>
     public string? Url { get; set; }
-
-    /// <summary>
-    /// Scoring generated for the evaluation.
-    /// Only populated if extraction succeeded.
-    /// </summary>
-    [Obsolete("Moved to Outputs")]
-    public EvaluationScoring? Scoring { get; set; }
 
     /// <summary>
     /// Unique identifiers of the reports for this verification.

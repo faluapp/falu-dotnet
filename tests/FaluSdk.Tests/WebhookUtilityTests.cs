7ba11e2f521c384af18bdaf41542b88ddc1c2d7d
Maxwell Weru <mburumaxwell@gmail.com>
2022-07-20 12:08:59 +0300

    Simpler use of timestamp in WebhookUtilityTests

--- tests/FaluSdk.Tests/WebhookUtilityTests.cs
+++ tests/FaluSdk.Tests/WebhookUtilityTests.cs
@@ -9,7 +9,7 @@ public class WebhookUtilityTests
     private const string Secret1 = "webhook_secret_expired";
     private const string Signature0 = "a950948ee5a9ce80d6d0d250dba2723be4a62319d936b280992f804ee9b35566";
     private const string Signature1 = "684ed18f8c9ca820c9fd079b83ed1fc5ddbf70e4e512b2ca5a2761a72c402a21";
-    private const long KnownTimestamp = 1658299746;
+    private static readonly DateTimeOffset KnownNow = DateTimeOffset.FromUnixTimeSeconds(1658299746);
     private static readonly string[] KnownSignatures = {
         $"t=1658299746,sha256={Signature0}",
         $"t=1658299746,sha256={Signature0},sha256={Signature1}",
@@ -18,27 +18,27 @@ public class WebhookUtilityTests
     [Fact]
     public async Task ValidateSignature_Works()
     {
-        var timestamp = DateTimeOffset.FromUnixTimeSeconds(KnownTimestamp).AddSeconds(100);
+        var now = KnownNow.AddSeconds(100);
         var json = await TestSamples.GetCloudEventAsStringAsync();
-        WebhookUtility.ValidateSignature(json, KnownSignatures[0], Secret0, now: timestamp);
+        WebhookUtility.ValidateSignature(json, KnownSignatures[0], Secret0, now: now);
     }
 
     [Fact]
     public async Task ValidateSignature_Works_With_RolledSecrets()
     {
-        var timestamp = DateTimeOffset.FromUnixTimeSeconds(KnownTimestamp).AddSeconds(100);
+        var now = KnownNow.AddSeconds(100);
         var json = await TestSamples.GetCloudEventAsStringAsync();
 
-        WebhookUtility.ValidateSignature(json, KnownSignatures[1], Secret0, now: timestamp);
-        WebhookUtility.ValidateSignature(json, KnownSignatures[1], Secret1, now: timestamp);
+        WebhookUtility.ValidateSignature(json, KnownSignatures[1], Secret0, now: now);
+        WebhookUtility.ValidateSignature(json, KnownSignatures[1], Secret1, now: now);
     }
 
     [Fact]
     public async Task ValidateSignature_Rejects_OldTimestamp()
     {
-        var timestamp = DateTimeOffset.FromUnixTimeSeconds(KnownTimestamp) + TimeSpan.FromSeconds(400);
+        var now = KnownNow.AddSeconds(400);
         var json = await TestSamples.GetCloudEventAsStringAsync();
-        var exception = Assert.Throws<FaluException>(() => WebhookUtility.ValidateSignature(json, KnownSignatures[0], Secret0, now: timestamp));
+        var exception = Assert.Throws<FaluException>(() => WebhookUtility.ValidateSignature(json, KnownSignatures[0], Secret0, now: now));
         Assert.Equal("The webhook cannot be processed because the current timestamp is outside of the allowed tolerance.", exception.Message);
     }
 

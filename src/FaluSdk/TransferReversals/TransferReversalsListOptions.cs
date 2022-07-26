9799c70e53fc3213b5156b8bea4aca531a69553f
Maxwell Weru <mburumaxwell@gmail.com>
2022-07-26 08:00:04 +0300

    Added filter by reason when listing transfer reversals

--- src/FaluSdk/TransferReversals/TransferReversalsListOptions.cs
+++ src/FaluSdk/TransferReversals/TransferReversalsListOptions.cs
@@ -8,10 +8,14 @@ public record TransferReversalsListOptions : BasicListOptionsWithMoney
     /// <summary>Filter options for <see cref="TransferReversal.Status"/> property.</summary>
     public List<string>? Status { get; set; }
 
+    /// <summary>Filter options for <see cref="TransferReversal.Reason"/> property.</summary>
+    public List<string>? Reason { get; set; }
+
     /// <inheritdoc/>
     internal override void Populate(QueryValues values)
     {
         base.Populate(values);
-        values.Add("status", Status);
+        values.Add("status", Status)
+              .Add("reason", Reason);
     }
 }

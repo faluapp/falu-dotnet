270f4b0151e725af59b64f6f0e1c0dabbfa9a625
Maxwell Weru <mburumaxwell@gmail.com>
2022-09-16 12:30:46 +0300

    Update descriptions for properties in EvaluationReportConsent and IdentityVerificationReportConsent

--- src/FaluSdk/IdentityVerificationReports/IdentityVerificationReportConsent.cs
+++ src/FaluSdk/IdentityVerificationReports/IdentityVerificationReportConsent.cs
@@ -4,19 +4,17 @@
 public class IdentityVerificationReportConsent
 {
     /// <summary>
-    /// The timestamp marking when the user gave consent for the identity verification to be done.
+    /// Time at which the user gave consent.
     /// </summary>
     public DateTimeOffset Date { get; set; }
 
     /// <summary>
-    /// The IP address from which the user gave consent for the identity verification to be done.
+    /// IP address from which the user gave consent.
     /// </summary>
-    /// <example>::ffff:127.0.0.1</example>
     public string? IP { get; set; }
 
     /// <summary>
-    /// The user agent of the browser from which the user gave consent for the identity verification to be done.
+    /// User agent of the device (e.g. browser) from which the user gave consent.
     /// </summary>
-    /// <example>Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36</example>
     public string? UserAgent { get; set; }
 }

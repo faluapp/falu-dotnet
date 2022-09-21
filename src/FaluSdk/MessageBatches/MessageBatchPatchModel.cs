430681c990912ba1e377d587bc709fc8d477d298
Maxwell Weru <mburumaxwell@gmail.com>
2022-09-21 18:14:40 +0300

    Added Description to Message Batches

--- src/FaluSdk/MessageBatches/MessageBatchPatchModel.cs
+++ src/FaluSdk/MessageBatches/MessageBatchPatchModel.cs
@@ -5,8 +5,11 @@ namespace Falu.MessageBatches;
 /// <summary>
 /// A model representing details that can be changed about a message batch.
 /// </summary>
-public class MessageBatchPatchModel : IHasMetadata
+public class MessageBatchPatchModel : IHasDescription, IHasMetadata
 {
+    /// <inheritdoc/>
+    public string? Description { get; set; }
+
     /// <inheritdoc/>
     public Dictionary<string, string>? Metadata { get; set; }
 }

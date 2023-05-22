0c29114d450584ee31c770ffc1e8e84663667658
Maxwell Weru <mburumaxwell@gmail.com>
2023-05-22 12:24:53 +0300

    Remove Tingle.Extensions.Processing in tests

--- tests/FaluSdk.Tests/TestSamples.cs
+++ tests/FaluSdk.Tests/TestSamples.cs
@@ -1,6 +1,4 @@
-﻿using Tingle.Extensions.Processing;
-
-namespace Falu.Tests;
+﻿namespace Falu.Tests;
 
 internal class TestSamples
 {
@@ -14,4 +12,26 @@ internal class TestSamples
 
     public static Task<string> GetCloudEventAsStringAsync() => GetAsStringAsync("cloud_event.json");
     public static Stream GetCloudEventAsStreamAsync() => GetAsStreamAsync("cloud_event.json");
+}
+
+internal static class EmbeddedResourceHelper
+{
+    public static Stream? GetResourceAsStream<T>(string resourceName) => typeof(T).Assembly.GetManifestResourceStream(resourceName);
+
+    public static async Task<string?> GetResourceAsStringAsync<T>(string resourceName)
+    {
+        var st = GetResourceAsStream<T>(resourceName);
+        if (st is null) return null;
+        using (st)
+        {
+            using var reader = new StreamReader(st);
+            return await reader.ReadToEndAsync().ConfigureAwait(false);
+        }
+    }
+
+    public static Task<string?> GetResourceAsStringAsync<T>(string folder, string fileName)
+        => GetResourceAsStringAsync<T>(string.Join(".", typeof(T).Namespace, folder, fileName));
+
+    public static Stream? GetResourceAsStream<T>(string folder, string fileName)
+        => GetResourceAsStream<T>(string.Join(".", typeof(T).Namespace, folder, fileName));
 }

ce63736dd7ae6fcda0638a4cbd6228c5d441ca4d
Maxwell Weru <mburumaxwell@gmail.com>
2023-07-12 09:43:30 +0300

    Added support for listing and retrieving temporary keys

--- src/FaluSdk/TemporaryKeys/TemporaryKeysServiceClient.cs
+++ src/FaluSdk/TemporaryKeys/TemporaryKeysServiceClient.cs
@@ -4,13 +4,47 @@ using SC = Falu.Serialization.FaluSerializerContext;
 namespace Falu.TemporaryKeys;
 
 ///
-public class TemporaryKeysServiceClient : BaseServiceClient<TemporaryKey>
+public class TemporaryKeysServiceClient : BaseServiceClient<TemporaryKey>,
+                                          ISupportsListing<TemporaryKey, TemporaryKeysListOptions>,
+                                          ISupportsRetrieving<TemporaryKey>
 {
     ///
     public TemporaryKeysServiceClient(HttpClient backChannel, FaluClientOptions options) : base(backChannel, options) { }
 
     /// <inheritdoc/>
     protected override string BasePath => "/v1/temporary_keys";
+
+    /// <summary>List temporary keys.</summary>
+    /// <inheritdoc/>
+    public virtual Task<ResourceResponse<List<TemporaryKey>>> ListAsync(TemporaryKeysListOptions? options = null,
+                                                                        RequestOptions? requestOptions = null,
+                                                                        CancellationToken cancellationToken = default)
+    {
+        return ListResourcesAsync(options, requestOptions, cancellationToken);
+    }
+
+    /// <summary>List temporary keys recursively.</summary>
+    /// <inheritdoc/>
+    public IAsyncEnumerable<TemporaryKey> ListRecursivelyAsync(TemporaryKeysListOptions? options = null,
+                                                               RequestOptions? requestOptions = null,
+                                                               CancellationToken cancellationToken = default)
+    {
+        return ListResourcesRecursivelyAsync(options, requestOptions, cancellationToken);
+    }
+
+    /// <summary>
+    /// Retrieve a temporary key.
+    /// </summary>
+    /// <param name="id">Unique identifier for the temporary key</param>
+    /// <param name="options">Options to use for the request.</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public virtual Task<ResourceResponse<TemporaryKey>> GetAsync(string id,
+                                                                 RequestOptions? options = null,
+                                                                 CancellationToken cancellationToken = default)
+    {
+        return GetResourceAsync(id, options, cancellationToken);
+    }
 
     /// <summary>
     /// Create a temporary key.
--- tests/FaluSdk.Tests/Clients/TemporaryKeysServiceClientTests.cs
+++ tests/FaluSdk.Tests/Clients/TemporaryKeysServiceClientTests.cs
@@ -19,6 +19,75 @@ public class TemporaryKeysServiceClientTests : BaseServiceClientTests<TemporaryK
 
     [Theory]
     [MemberData(nameof(RequestOptionsData))]
+    public async Task GetAsync_Works(RequestOptions options)
+    {
+        var handler = GetAsync_Handler(options);
+
+        await TestAsync(handler, async (client) =>
+        {
+            var response = await client.TemporaryKeys.GetAsync(Data!.Id!, options);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.NotNull(response.Resource);
+            Assert.Equal(Data!.Id, response.Resource!.Id);
+        });
+    }
+
+    [Theory]
+    [MemberData(nameof(RequestOptionsWithHasContinuationTokenData))]
+    public async Task ListAsync_Works(RequestOptions options, bool hasContinuationToken)
+    {
+        var handler = ListAsync_Handler(hasContinuationToken, options);
+
+        await TestAsync(handler, async (client) =>
+        {
+            var opt = new TemporaryKeysListOptions
+            {
+                Count = 1
+            };
+
+            var response = await client.TemporaryKeys.ListAsync(opt, options);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.NotNull(response.Resource);
+            Assert.Single(response.Resource);
+
+            if (hasContinuationToken) Assert.NotNull(response.ContinuationToken);
+            else Assert.Null(response.ContinuationToken);
+
+            var ev = response!.Resource!.Single();
+
+            Assert.Equal(Data!.Id, ev.Id);
+        });
+    }
+
+    [Theory]
+    [MemberData(nameof(RequestOptionsData))]
+    public async Task ListRecursivelyAsync_Works(RequestOptions options)
+    {
+        var handler = ListAsync_Handler(options: options);
+
+        await TestAsync(handler, async (client) =>
+        {
+            var opt = new TemporaryKeysListOptions
+            {
+                Count = 1
+            };
+
+            var results = new List<TemporaryKey>();
+
+            await foreach (var item in client.TemporaryKeys.ListRecursivelyAsync(opt, options))
+            {
+                results.Add(item);
+            }
+
+            Assert.Single(results);
+            var ev = results.Single();
+            Assert.Equal(Data!.Id, ev.Id);
+        });
+    }
+
+    [Theory]
+    [MemberData(nameof(RequestOptionsData))]
     public async Task CreateAsync_Works(RequestOptions options)
     {
         var handler = CreateAsync_Handler(options);
